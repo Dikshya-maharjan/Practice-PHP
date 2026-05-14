0dd2e8ae7aee029c07e8d9a294ee66525845149f
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-05-14 12:53:20 +0545

    feat:update HomePage.php file

--- Task1-Day7/HomePage/HomePage.php
+++ Task1-Day7/HomePage/HomePage.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include "../Database/Database.php";
 if(!isset($_SESSION['email'])){
     //
     header("Location: LoginPage.html");
@@ -26,11 +26,41 @@ if(isset($_SESSION['message'])){
 
 <p>You are logged in as: <?php echo $_SESSION['email']; ?></p>
 <p> You logged in as :<?php echo $_SESSION['role'];?></p>
-<?php if ($_SESSION['role'] === 'superadmin') { ?>
+<?php 
+if ($_SESSION['role'] === 'superadmin') { ?>
     <a href="../AssignRole/assign_role.php">
         <button>Go to Assign Role</button>
     </a>
 <?php } ?>
+<!-- display menu -->
 
+<h3>Menus</h3>
+ <?php
+ if(!empty($_SESSION['role_id'])){
+$sql = "
+SELECT m.name
+FROM menus m
+JOIN menu_role mr ON m.id = mr.menu_id
+
+WHERE mr.role_id = :role_id
+";
+$stmt = $pdo->prepare($sql);
+$stmt->bindParam(':role_id', $_SESSION['role_id']);
+$stmt->execute();
+$menus = $stmt->fetchAll(PDO::FETCH_ASSOC);
+if ($menus) {
+        foreach ($menus as $menu){
+            echo "<li>{$menu['name']}</li>";
+        }
+    } else {
+        echo "No menus found for this role.";
+    }
+
+}
+?>
+
+<a href="../Logout/logout.php" onclick="return confirm('Are you sure you want to logout?');">
+    <button type="button">Logout</button>
+</a>
 </body>
 </html>
