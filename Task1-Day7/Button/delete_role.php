099f3a94b66fa15032e3d25ea440d06548cfda11
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-05-18 16:58:08 +0545

    feat:update delete_role.php

--- Task1-Day7/Button/delete_role.php
+++ Task1-Day7/Button/delete_role.php
@@ -4,13 +4,10 @@ include "../Database/Database.php";
 $user_id = $_GET['user_id'];
 $role_id = $_GET['role_id'];
 
-$sql = "DELETE FROM role_user WHERE user_id = :user_id AND role_id = :role_id";
+$sql = "DELETE FROM role_user WHERE user_id =? AND role_id = ?";
 $stmt = $pdo->prepare($sql);
 
-$stmt->execute([
-    ':user_id' => $user_id,
-    ':role_id' => $role_id
-]);
+$stmt->execute([$user_id,$role_id]);
 
 header("Location: assign_role_users.php");
 exit();
