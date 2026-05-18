d97de72f7ef93f5640bae65613804a7bc7aafbd6
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-05-18 15:23:00 +0545

    feat:update header.php file

--- Task1-Day7/Header/Header.php
+++ Task1-Day7/Header/Header.php
@@ -1,26 +1,26 @@
 <?php
 include "../Database/Database.php";
-if(!isset($_SESSION['email'])){
-    //
+
+if (!isset($_SESSION['email'])) {
     header("Location: LoginPage.html");
     exit();
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Header</title>
-    <link rel="styleshee" href="../HomePage/Homepage.css"/>
-</head>
-<body>
-    <div class="mainContainer">
 
-        <h2>Welcome <?php 
-        
-        echo $_SESSION['email'];?></h2>
-    </div>
+</head>
+
+<body>
+
+<div class="mainContainer">
+    <h2>Welcome <?php echo $_SESSION['email']; ?></h2>
+</div>
 
 </body>
 </html>
