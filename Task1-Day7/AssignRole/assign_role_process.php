cc8828ef7e9b55868b2eb061ac4fd1a475d7c012
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-05-22 16:04:00 +0545

    feat:update assign_role_process.php file

--- Task1-Day7/AssignRole/assign_role_process.php
+++ Task1-Day7/AssignRole/assign_role_process.php
@@ -2,24 +2,24 @@
 session_start();
 include '../Database/Database.php';
 
-
-if (!isset($_SESSION['role']) || $_SESSION['role'] != 'superadmin') {
+if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'superadmin') {
     die("Access Denied");
 }
-$user_id =$_POST['user_id'];
-$role_id =$_POST['role_id'];
 
-$sql = "INSERT INTO role_user (user_id, role_id)
-        VALUES (:user_id, :role_id)
-        ON DUPLICATE KEY UPDATE role_id = :role_id";
+$user_id = $_POST['user_id'];
+$role_id = $_POST['role_id'];
+
+$sql = "
+INSERT INTO role_user (user_id, role_id)
+VALUES (:user_id, :role_id)
+ON DUPLICATE KEY UPDATE role_id = VALUES(role_id)
+";
 
 $stmt = $pdo->prepare($sql);
-$stmt->bindParam(':user_id',$user_id);
-$stmt->bindParam(':role_id',$role_id);
+$stmt->bindParam(':user_id', $user_id);
+$stmt->bindParam(':role_id', $role_id);
 $stmt->execute();
 
-echo "<script>
-alert('Role assigned successfully');
-window.location.href='../AssignRole/assign_role.php';
-</script>";
+header("Location: assign_role.php");
+exit();
 ?>
