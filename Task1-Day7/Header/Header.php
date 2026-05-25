f3e355eb12ab4d9d47de09b9810b14d9b2ad1d64
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-05-25 14:24:13 +0545

    feat(ui): show dropdown button only on homepage

--- Task1-Day7/Header/Header.php
+++ Task1-Day7/Header/Header.php
@@ -7,6 +7,8 @@ if (!isset($_SESSION['email'])) {
     header("Location: /InternPHP/Task1-Day7/LoginPage.html");
     exit();
 }
+    $currentPage = basename($_SERVER['PHP_SELF']);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -31,7 +33,55 @@ if (!isset($_SESSION['email'])) {
             </h3>
     
         </div>
+    <div class="nav-right">
+
+    <!-- ROLE DROPDOWN -->
+    <div class="btn-group me-3">
+        <?php
+        if($currentPage=="HomePage.php"){ ?>
+        
+        <button type="button"
+        class="btn btn-info dropdown-toggle"
+        data-bs-toggle="dropdown"
+        aria-expanded="false">
+        Select Role
+    </button>
     
+    <ul class="dropdown-menu">
+        
+        <?php if(empty($roles)) { ?>
+        
+        <li>
+            <span class="dropdown-item text-muted">
+                No roles assigned
+            </span>
+        </li>
+        
+        <?php } else { ?>
+        
+        <?php foreach($roles as $role) { ?>
+        
+        <li>
+              <a class="dropdown-item"
+              href="/InternPHP/Task1-Day7/Homepage/Homepage.php?role_id=<?php echo $role['id']; ?>">
+              
+              <?php echo htmlspecialchars($role['role_name']); ?>
+              
+            </a>
+        </li>
+        
+        <?php } ?>
+        
+        <?php } ?>
+        
+    </ul>
+    <?php }?>
+
+    </div>
+
+ 
+
+</div>
     </div>
 </body>
 </html>
