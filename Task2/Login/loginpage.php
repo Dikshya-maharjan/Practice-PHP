c997baa6e6e9f5979ed36e5f5d2c17bc5aa5cd9f
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-09 13:46:10 +0545

    feat: add loginpage.php file

---
+++ Task2/Login/loginpage.php
@@ -0,0 +1,31 @@
+<?php
+include '../Database/database.php';
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+<body>
+    <main>
+        <h1>Log into your account</h1>
+        <section>
+        <form method="POST" action="/">
+            <label>Username</label>
+            <input type="text" name="name" placeholder='name'/><br/>
+            <label>Email</label>
+            <input type="email" name="email" placeholder='email@gmail.com'/><br/>
+            <label>Password</label>
+            <input type="password" name="password" /><br/>
+            <input type="submit" name="submit" id="login" value="Login"/>
+<p>Don't have an account?<a href="../Signup/SignupPage.html">Sign up</a></p>            
+
+        </form>
+
+</section>
+</main>
+</body>
+</html>
