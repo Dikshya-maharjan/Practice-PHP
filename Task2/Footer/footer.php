2a81c7cb07884fdff869a9ef4af0e6e60911bb13
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-09 16:16:43 +0545

    feat:add footer.php file

---
+++ Task2/Footer/footer.php
@@ -0,0 +1,19 @@
+<?php
+// No need to start session here unless required
+?>
+
+<footer>
+    <div class="footer-container">
+
+        <p>&copy; <?php echo date("Y"); ?> Blog System. All Rights Reserved.</p>
+
+        <p>
+            Built with  using PHP, MySQL, HTML & CSS
+        </p>
+
+
+    </div>
+</footer>
+
+</body>
+</html>
