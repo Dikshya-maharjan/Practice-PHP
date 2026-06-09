40dbdc092a849c4d8b33e5b9fa61f9abaf8181f8
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-09 14:00:48 +0545

    feat:add form section in file

--- Task2/Login/loginpage.php
+++ Task2/Login/loginpage.php
@@ -7,6 +7,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login.css">
     <title>Login Page</title>
 </head>
 <body>
@@ -14,11 +15,11 @@ session_start();
         <h1>Log into your account</h1>
         <section>
         <form method="POST" action="/">
-            <label>Username</label>
+            <label>Username</label><br/>
             <input type="text" name="name" placeholder='name'/><br/>
-            <label>Email</label>
+            <label>Email</label><br/>
             <input type="email" name="email" placeholder='email@gmail.com'/><br/>
-            <label>Password</label>
+            <label>Password</label><br/>
             <input type="password" name="password" /><br/>
             <input type="submit" name="submit" id="login" value="Login"/>
 <p>Don't have an account?<a href="../Signup/SignupPage.html">Sign up</a></p>            
